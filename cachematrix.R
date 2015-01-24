98dd9cddc1eed0470a70f1db28efff6e83cc06b1
verdhan <verdhan.mba@gmail.com>
2015-01-25 02:14:00 +0530

    More changes
    Changes

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,13 @@
 ## First make the makeCacheMatrix function which takes matrix as an input parameter
 
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
+  m<-NULL #initializing the value with NULL
   set<-function(y){
     x<<-y
     m<<-NULL
   }
   get<-function() x
-  setmatrix<-function(solve) m<<- solve
+  setmatrix<-function(solve) m<<- solve #solve gives the inverse
   getmatrix<-function() m
   list(set=set, get=get,
        setmatrix=setmatrix,
@@ -18,14 +18,15 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Make the cacheSolve function which takes matrix as an input parameter
+## Make the cacheSolve function which takes matrix as an input parameter, it will first check
+##if the value is present in cache and if not will calculate it.if the value is present it will return that
 
 cacheSolve <- function(x, ...) {
   m<-x$getmatrix()
-  if(!is.null(m))
+  if(!is.null(m)) #if the vale of m is not null
   {
     message("getting cached data")
-    return(m)
+    return(m) #returm m if the vale of m is not null else we need to calcualte it
   }
   matrix<-x$get()
   m<-solve(matrix, ...)
